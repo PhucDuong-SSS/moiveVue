b787cfe611e27232aba3acc5e6c36d777bca774f
PhucDuong-SSS <phucdnDev@gmail.com>
2021-06-11 15:21:18 +0700

    header search

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="bg-gray-900">
-    Home
+  <div class="container mx-auto " >
+    <div class="flex justify-between  border-b border-gray-500">
+       <Navbar/>
+    <Searchbar/>
+    </div>   
+    <PopularMovies/>
   </div>
 </template>
 
 <script>
+import Navbar from './header/Navbar.vue'
+import Searchbar from './header/Searchbar.vue'
+import PopularMovies from './PopularMovie.vue'
 export default {
-
+name: 'home',
+components: {
+  Navbar,
+  Searchbar,
+  PopularMovies
+}
 }
 </script>
 
---
+++ src/components/header/Searchbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mt-5 flex">
+    <input type="text" class="rounded-full bg-gray-600 px-5 w-50 h-10 mr-3 focus:outline-none " placeholder="Search ...">
+    <img src="@/assets/images/avatar.jpg" alt=""  class="h-10 rounded-full">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
